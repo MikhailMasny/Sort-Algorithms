cc72b90ab4fe7409f1f9e8f20b702242e5057cb7
Михаил Масный <masny.mikhail@gmail.com>
2019-04-22 20:34:46 +0300

    Добавьте файлы проекта.

---
+++ SortAlgorithms.BL/AlgorithmsBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms.BL
+{
+    public class AlgorithmsBase<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public void Swap(int positionA, int postitonB)
+        {
+            if(positionA < Items.Count && postitonB < Items.Count)
+            {
+                var temp = Items[positionA];
+                Items[positionA] = Items[postitonB];
+                Items[postitonB] = temp;
+            }
+        }
+    }
+}
