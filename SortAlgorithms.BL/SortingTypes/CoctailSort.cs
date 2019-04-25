de2314d1dfb6caa8109595331e42285d1a313315
Михаил Масный <masny.mikhail@gmail.com>
2019-04-25 23:49:44 +0300

    Some bug fix.

--- SortAlgorithms.BL/SortingTypes/CoctailSort.cs
+++ SortAlgorithms.BL/SortingTypes/CoctailSort.cs
@@ -22,41 +22,25 @@ namespace SortAlgorithms.BL
 
             while(left < right)
             {
-                var sc = SwapCount;
-
                 for(int i = left; i < right; i++)
                 {
                     if(Items[i].CompareTo(Items[i+1]) == 1)
                     {
                         Swap(i, i + 1);
-
-                        ComparisonCount++;
                     }
                 }
 
                 right--;
-
-                if (sc == SwapCount)
-                {
-                    break;
-                }
 
                 for (int i = right; i > left; i--)
                 {
                     if(Items[i].CompareTo(Items[i - 1]) == -1)
                     {
                         Swap(i, i - 1);
-
-                        ComparisonCount++;
                     }
                 }
 
                 left++;
-
-                if (sc == SwapCount)
-                {
-                    break;
-                }
             }
         }
     }
