87ec0fb16a366d833d71713d71547ac06f2b10a4
Михаил Масный <masny.mikhail@gmail.com>
2019-04-25 22:53:05 +0300

    Implemented cocktail sorting. Added MSTestv2 test.

---
+++ SortAlgorithms.BLTests/CoctailSortTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortAlgorithms.BL.Tests
+{
+    [TestClass()]
+    public class CoctailSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            // Arrange
+            var coctail = new CoctailSort<int>();
+            var rnd = new Random();
+            var items = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+
+            coctail.Items.AddRange(items);
+
+            var sorted = items.ToArray().OrderBy(x => x).ToArray();
+
+            // Act
+            coctail.Sort();
+
+            // Assest
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(sorted[i], coctail.Items[i]);
+            }
+        }
+    }
+}
---
+++ SortAlgorithms.BLTests1/CoctailSortTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortAlgorithms.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms.BL.Tests
+{
+    [TestClass()]
+    public class CoctailSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
