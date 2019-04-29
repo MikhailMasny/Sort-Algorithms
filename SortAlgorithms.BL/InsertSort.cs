0d942b5cd643eba95ad0cfc591879f4bd64444b4
Михаил Масный <masny.mikhail@gmail.com>
2019-04-29 22:58:08 +0300

    Implement insertion sort algorithm.

---
+++ SortAlgorithms.BL/InsertSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SortAlgorithms.BL
+{
+    public class InsertSort<T> : AlgorithmsBase<T> where T : IComparable
+    {
+        protected override void MakeSort()
+        {
+            for(int i = 1; i< Items.Count; i++)
+            {
+                var temp = Items[i];
+                var j = i;
+                while(j > 0 && temp.CompareTo(Items[j - 1]) == -1)
+                {
+                    Items[j] = Items[j - 1];
+                }
+                Items[j] = temp;
+            }
+        }
+    }
+}
