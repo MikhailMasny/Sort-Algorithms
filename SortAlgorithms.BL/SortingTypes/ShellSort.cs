30c144023b46dd123b154acef1980ed15210a4ed
Михаил Масный <masny.mikhail@gmail.com>
2019-05-02 20:45:54 +0300

    Shell sorting is implemented.

---
+++ SortAlgorithms.BL/SortingTypes/ShellSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms.BL
+{
+    class ShellSort<T> : AlgorithmsBase<T> where T : IComparable
+    {
+        protected override void MakeSort()
+        {
+            var step = Items.Count / 2;
+
+            while(step > 0)
+            {
+                for (int i = step; i < Items.Count; i++)
+                {
+                    int j = i;
+                    while ((j >= step) && Items[j - step].CompareTo(Items[j]) == 1)
+                    {
+                        Swap(j - step, j);
+                        j -= step;
+                    }
+                }
+
+                step /= 2;
+            }
+        }
+    }
+}
