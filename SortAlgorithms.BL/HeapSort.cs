71c79866e8673a365a5412b65101d43e4ba257cc
Михаил Масный <masny.mikhail@gmail.com>
2019-05-06 21:30:02 +0300

    Added data structures Node, Tree and Heap. Implemented pyramid sorting (binary heap). Implemented binary search tree sorting.

---
+++ SortAlgorithms.BL/HeapSort.cs
@@ -0,0 +1,15 @@
+﻿using SortAlgorithms.BL.DataStructures;
+using System;
+
+namespace SortAlgorithms.BL
+{
+    public class HeapSort<T> : AlgorithmsBase<T> where T : IComparable
+    {
+        protected override void MakeSort()
+        {
+            var heap = new Heap<T>(Items);
+            var sorted = heap.Order();
+            Items = sorted;
+        }
+    }
+}
---
+++ SortAlgorithms.BL/TreeSort.cs
@@ -0,0 +1,19 @@
+﻿using SortAlgorithms.BL.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms.BL
+{
+    public class TreeSort<T> : AlgorithmsBase<T> where T : IComparable
+    {
+        protected override void MakeSort()
+        {
+            var tree = new Tree<T>(Items);
+            var sorted = tree.Inorder();
+            Items = sorted;
+        }
+    }
+}
