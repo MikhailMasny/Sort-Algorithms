4476bdf8c8b9997e516e470bf953c8af0b1825d2
Михаил Масный <masny.mikhail@gmail.com>
2019-05-28 21:47:34 +0300

    Added file with constants.

---
+++ SortAlgorithms.UI/Constants.cs
@@ -0,0 +1,58 @@
+﻿namespace SortAlgorithms.UI
+{
+    /// <summary>
+    /// Основные константы проекта.
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// Пузырьковая сортировка.
+        /// </summary>
+        public const string BubbleSort = "Bubble Sort";
+
+        /// <summary>
+        /// Шейкерная сортировка.
+        /// </summary>
+        public const string CoctailSort = "Coctail Sort";
+
+        /// <summary>
+        /// Сортировка вставками.
+        /// </summary>
+        public const string InsertionSort = "Insertion Sort"; 
+
+        /// <summary>
+        /// Сортировка Шелла.
+        /// </summary>
+        public const string ShellSort = "Shell Sort";
+
+        /// <summary>
+        /// Сортировка кучей.
+        /// </summary>
+        public const string HeapSort = "Heap Sort";
+
+        /// <summary>
+        /// Сотировка бинарным деревом.
+        /// </summary>
+        public const string TreeSort = "Tree Sort";
+
+        /// <summary>
+        /// Сортировка выбором.
+        /// </summary>
+        public const string SelectionSort = "Selection Sort";
+
+        /// <summary>
+        /// Гномья сортировка.
+        /// </summary>
+        public const string GnomeSort = "Gnome Sort";
+
+        /// <summary>
+        /// Сортировка вставками.
+        /// </summary>
+        public const string MergeSort = "Merge Sort";
+
+        /// <summary>
+        /// Быстрая сортировка.
+        /// </summary>
+        public const string QuickSort = "Quick Sort";
+    }
+}
